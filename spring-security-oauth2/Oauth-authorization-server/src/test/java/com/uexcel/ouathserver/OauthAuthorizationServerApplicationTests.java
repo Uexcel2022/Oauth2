34a9a7670e41975fc530daa637d5e03f7588ee6e
Uexcel2022 <excellenceudoka@gmail.com>
2023-11-13 11:29:59 +0100

    commit

---
+++ spring-security-oauth2/Oauth-authorization-server/src/test/java/com/uexcel/ouathserver/OauthAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uexcel.ouathserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthAuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
